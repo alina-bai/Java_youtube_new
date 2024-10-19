a517d7838be5da280d7c6ea233df8a3445a97831
Alina-bai <alina.baishukurova@gmail.com>
2024-10-19 21:06:12 +0400

    OOP in Java

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,20 @@
+
+package org.example;
+public class Car {
+    //So myCar has these atributes and ~
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2020;
+    String color = "blue";
+    double price = 50000.00;
+
+
+    // These methods:
+    void drive() {
+        System.out.println("You drive the car");
+    }
+
+    void brake() {
+        System.out.println("You step on brake");
+    }
+}
---
+++ src/main/java/org/example/OOP.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class OOP {
+    public static void main (String[] args) {
+
+         //Object an instance of a class that may contain atributes and methods.
+
+        Car myCar = new Car();
+        Car myCar2 = new Car();
+        System.out.println(myCar.model);
+        System.out.println(myCar.make);
+        System.out.println();
+        System.out.println(myCar2.model);
+        System.out.println(myCar2.make);
+    }
+}
+
