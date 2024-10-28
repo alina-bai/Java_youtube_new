0e469574c20706466cd06e4f19e5a4a23913059f
Alina-bai <alina.baishukurova@gmail.com>
2024-10-28 19:12:53 +0400

    Method Overriding

---
+++ src/main/java/org/example/MethodOverriding.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class MethodOverriding {
+    public static void main (String[] args) {
+
+        Animal animal =new Animal();
+        Dog dog = new Dog();
+        dog.speak();
+    }
+}
